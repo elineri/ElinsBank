b84152110e4659f5a0a055e78b4a17e480e68ee3
elineri <elin.ericstam@gmail.com>
2021-10-06 16:17:01 +0200

    Add project files.

---
+++ ElinsBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElinsBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till Elins bank!");
+        }
+    }
+}
