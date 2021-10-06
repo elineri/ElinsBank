26d35cf0acfb0769a7486037fe70fde985994641
elineri <elin.ericstam@gmail.com>
2021-10-06 16:19:01 +0200

    Lagt till användare och användares lösenord.

--- ElinsBank/Program.cs
+++ ElinsBank/Program.cs
@@ -7,6 +7,24 @@ namespace ElinsBank
         static void Main(string[] args)
         {
             Console.WriteLine("Välkommen till Elins bank!");
+
+            // Users
+            string[] user = new string[5];
+            user[0] = "elin.ericstam";
+            user[1] = "anas.alhussain";
+            user[2] = "tobias.landen";
+            user[3] = "malin.claesson";
+            user[4] = "fredrik.strandberg";
+
+            // User passwords
+            string[] userpin = new string[5];
+            userpin[0] = "1234";
+            userpin[1] = "1234";
+            userpin[2] = "1234";
+            userpin[3] = "1234";
+            userpin[4] = "1234";
+
+            
         }
     }
 }
